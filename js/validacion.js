58cbec473aafd959e8d6a4ac4d0aaa047ffae022
Ezequiel Marcenal <emarcenal67@gmail.com>
2025-08-13 13:55:20 -0300

    funcion

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function passwordEqual() {
+    document.addEventListener("DOMContentLoaded", function () {
+        const btn = document.getElementById("regBtn");
+
+        btn.addEventListener("click", function (event) {
+            const pass1 = document.getElementById("password1").value;
+            const pass2 = document.getElementById("password2").value;
+
+            if (pass1 !== pass2) {
+                event.preventDefault();
+                showAlertError();
+            } else {
+                showAlertSuccess();
+            }
+        });
+    });
+}
+passwordEqual();
